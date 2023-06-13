4592be9c1f192c780d12474d57577effe967ac65
Luis <lmr.87@protonmail.ch>
2023-06-13 16:47:00 +0200

    FloatingActionButtonSamples.kt

---
+++ app/src/main/java/com/lmr/ejemplosmaterial3jetpackcomposegoogle/samples/FloatingActionButtonSamples.kt
@@ -0,0 +1,126 @@
+package com.lmr.ejemplosmaterial3jetpackcomposegoogle.samples
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Sampled
+@Composable
+fun FloatingActionButtonSample() {
+    FloatingActionButton(
+        onClick = { /* do something */ },
+    ) {
+        Icon(Icons.Filled.Add, "Localized description")
+    }
+}
+
+@Preview
+@Sampled
+@Composable
+fun SmallFloatingActionButtonSample() {
+    SmallFloatingActionButton(
+        onClick = { /* do something */ },
+    ) {
+        Icon(Icons.Filled.Add, contentDescription = "Localized description")
+    }
+}
+
+@Preview
+@Sampled
+@Composable
+fun LargeFloatingActionButtonSample() {
+    LargeFloatingActionButton(
+        onClick = { /* do something */ },
+    ) {
+        Icon(
+            Icons.Filled.Add,
+            contentDescription = "Localized description",
+            modifier = Modifier.size(FloatingActionButtonDefaults.LargeIconSize),
+        )
+    }
+}
+
+@Preview
+@Sampled
+@Composable
+fun ExtendedFloatingActionButtonTextSample() {
+    ExtendedFloatingActionButton(onClick = { /* do something */ }) {
+        Text(text = "Extended FAB")
+    }
+}
+
+@Preview
+@Sampled
+@Composable
+fun ExtendedFloatingActionButtonSample() {
+    ExtendedFloatingActionButton(
+        onClick = { /* do something */ },
+        icon = { Icon(Icons.Filled.Add, "Localized description") },
+        text = { Text(text = "Extended FAB") },
+    )
+}
+
+/**
+ * He tenido que cambiar los imports, poner anotaciones y comentar un parámetro para que compile
+ */
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Sampled
+@Composable
+fun AnimatedExtendedFloatingActionButtonSample() {
+    val listState = rememberLazyListState()
+    // The FAB is initially expanded. Once the first visible item is past the first item we
+    // collapse the FAB. We use a remembered derived state to minimize unnecessary compositions.
+    val expandedFab by remember {
+        derivedStateOf {
+            listState.firstVisibleItemIndex == 0
+        }
+    }
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { /* do something */ },
+                expanded = expandedFab,
+                icon = { Icon(Icons.Filled.Add, "Localized Description") },
+                text = { Text(text = "Extended FAB") },
+            )
+        },
+        //isFloatingActionButtonDocked = false,
+        floatingActionButtonPosition = FabPosition.End,
+    ) {
+        LazyColumn(state = listState, modifier = Modifier.fillMaxSize()) {
+            for (index in 0 until 100) {
+                item {
+                    Text(
+                        text = "List item - $index",
+                        modifier = Modifier.padding(24.dp)
+                    )
+                }
+            }
+        }
+    }
+}
